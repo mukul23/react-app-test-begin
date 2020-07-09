06d914eb2d2de020931fb07eea1570eaee94df9c
Mukul Ishwar <mukul.ishwar23@gmail.com>
2020-07-09 21:19:16 +0530

    Added an h1
    Testing begin's deploymemt

--- src/App.js
+++ src/App.js
@@ -20,22 +20,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-        <p>Change me!</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Checking if you work or not</h1>
     </div>
   );
 }
