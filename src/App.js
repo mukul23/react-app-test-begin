bfa650604c2ccd6349990f5c4776a537a1c8987f
Mukul Ishwar <mukul.ishwar23@gmail.com>
2020-07-09 21:23:24 +0530

    Clean up the code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-/* global fetch */
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 const App = () => {
-  const [message, setMessage] = useState('...loading')
-
-  useEffect(() => {
-    async function fetchData () {
-      try {
-        let data = await (await fetch('/api')).json()
-        setMessage(data.message)
-      } catch (err) {
-        setMessage(err.message)
-      }
-    }
-    fetchData()
-  })
-
-  return (
-    <div className="App">
-     <h1>Checking if you work or not</h1>
+  return(
+    <div>
+      <h1>Checking if it works</h1>
     </div>
-  );
+  )
 }
 
 export default App;
